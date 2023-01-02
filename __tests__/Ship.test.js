653e3e3a0580ce4d722435d5c1d8b8814ba768eb
Lewis Smith <lewislbssmith212@gmail.com>
2023-01-02 14:54:01 +0000

    isolating with spies

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -11,8 +11,18 @@ describe('Ship', () => {
         let itinerary;
 
         beforeEach(() => {
-            dover = new Port('Dover');
-            calais = new Port('Calais');
+            dover = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                name: 'Dover',
+                ships: []
+            };
+            calais = {
+                addShip: jest.fn(),
+                removeShip: jest.fn(),
+                name: 'Calais',
+                ships: []
+            };
             itinerary = new Itinerary([dover, calais]);
             ship = new Ship(itinerary);
         });
@@ -25,7 +35,7 @@ describe('Ship', () => {
             it('can set sail', () => {
                 ship.setSail();
                 expect(ship.currentPort).toBeFalsy();
-                expect(dover.ships).not.toContain(ship);
+                expect(dover.removeShip).toHaveBeenCalledWith(ship);
             });
             it('can\'t sail further than its itinerary', () => {
                 ship.setSail();
@@ -38,10 +48,10 @@ describe('Ship', () => {
                 ship.dock();
                 
                 expect(ship.currentPort).toBe(calais);
-                expect(calais.ships).toContain(ship);
+                expect(calais.addShip).toHaveBeenCalledWith(ship);
             });
             it('gets added to port on instantiation', () => {
-                expect(dover.ships).toContain(ship);
+                expect(dover.addShip).toHaveBeenCalledWith(ship);
             });
     });
 });
