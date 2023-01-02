8ad8301952d193a3436b398974c5ccddb75c7a2e
Lewis Smith <lewislbssmith212@gmail.com>
2023-01-02 14:59:59 +0000

    isolating with stubs

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,7 +1,5 @@
 /* globals describe it expect */
 const Ship = require('../src/Ship.js');
-const Port = require('../src/Port.js');
-const Itinerary = require('../src/itinerary.js');
 
 describe('Ship', () => {
     describe('with ports and an itinerary', () => {
@@ -23,7 +21,9 @@ describe('Ship', () => {
                 name: 'Calais',
                 ships: []
             };
-            itinerary = new Itinerary([dover, calais]);
+            itinerary = {
+                ports: [dover, calais]
+            };
             ship = new Ship(itinerary);
         });
             it('can be instantiated', () => {
